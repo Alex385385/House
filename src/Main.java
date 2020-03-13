65b32058bad5a0e5f205a835b75bd72d4a1c6225
Alejandro Millan <alejandro_millan@ymail.com>
2020-03-13 00:50:15 -0700

    Finished house using shapes

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 import javafx.application.Application;
-import javafx.geometry.Pos;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Polygon;
 import javafx.scene.shape.Rectangle;
@@ -12,16 +11,30 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Rectangle bigRec = new Rectangle();
-        Circle window1 = new Circle();
-        Circle window2 = new Circle();
-        Polygon roof = new Polygon();
-        Rectangle door = new Rectangle();
+        Rectangle house = new Rectangle(120, 200, 510, 280);
+        house.setFill(Color.web("#F7C18E"));
 
-        VBox vbox1 = new VBox();
+        Circle window1 = new Circle(230, 350, 40);
+        window1.setFill(Color.WHITE);
 
-        Scene scene = new Scene(vbox1, 550, 500);
-        primaryStage.setTitle("Skateboard Designer");
+        Circle window2 = new Circle(530, 350, 40);
+        window2.setFill(Color.WHITE);
+
+        Polygon roof = new Polygon(375, 50,
+                                    100, 200,
+                                    650, 200);
+        roof.setFill(Color.web("#653608"));
+
+        Rectangle door = new Rectangle(340, 330, 80, 150);
+        door.setFill(Color.web("#910F00"));
+
+
+        Pane pane = new Pane(roof, house, door, window1, window2);
+
+        pane.setStyle("-fx-background-color: #F8FEFF");
+
+        Scene scene = new Scene(pane, 750, 600);
+        primaryStage.setTitle("House");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
